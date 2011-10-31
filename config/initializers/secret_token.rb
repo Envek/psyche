dda16816e4b2f3555bf5d518260155e6c711c424
Novikov Andrey <envek@envek.name>
2011-10-31 23:25:00 +1100

    Initial commit: rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Psyche::Application.config.secret_token = '2927f4649c58fcdcf0cfa84944fcf9de4c4ab39e35bb8d06d4298abb77988aff69112ec36949b7181c5ddea383a1e74173f79dd488879833954c9537daadad1f'
