41f0260523a53057fd268a70e435f77236fcab49
Novikov Andrey <envek@envek.name>
2011-11-16 21:22:24 +1000

    Enhanced patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,15 @@
+# encoding: UTF-8
 class Patient < ActiveRecord::Base
   has_many :examinations
   belongs_to :district
+
+  default_scope order("surname ASC, name ASC, patronymic ASC")
+
+  def fullname
+    "#{surname} #{name} #{patronymic}"
+  end
+
+  def shortname
+    "#{surname} #{name[0]}. #{patronymic[0]}."
+  end
 end
