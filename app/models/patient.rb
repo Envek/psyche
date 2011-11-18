494b4ab0313ea53ba5fcaa09d6feb3e411409232
Novikov Andrey <envek@envek.name>
2011-11-18 15:08:05 +1000

    Added shortinfo for patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,6 +2,7 @@
 class Patient < ActiveRecord::Base
   has_many :examinations
   belongs_to :district
+  has_many :schedules
 
   default_scope order("surname ASC, name ASC, patronymic ASC")
 
@@ -11,6 +12,10 @@ class Patient < ActiveRecord::Base
 
   def shortname
     "#{surname} #{name[0]}. #{patronymic[0]}."
+  end
+
+  def shortinfo
+    I18n.t("activerecord.attributes.patient.shortinfo", :name => self.fullname, :birth => self.date_of_birth.try(:year), :district => self.district.try(:title))
   end
 
   def examination
